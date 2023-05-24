05976ac2d14f0c8fe3b2835c72679778fdfd319d
Maria Popova <marypoppens14@gmail.com>
2023-05-24 16:23:54 +0300

    task number 1

---
+++ task1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Написать цикл, который принимает на вход два числа (А и В) 
+// возводит число А в натуральную степень В
+
+int newNumber(int a, int b)
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+    return result;
+}
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+int NewNumber = (int)Math.Pow(a, b);
+Console.WriteLine("Итог: " + NewNumber);
